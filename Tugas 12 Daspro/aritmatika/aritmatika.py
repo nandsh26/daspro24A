c78b8db256c803484c93c5dcd5dc3cf22d664e66
nandsh26 <nandasawaliah001@gmail.com>
2025-05-27 21:41:00 +0700

    Add files via upload

---
+++ Tugas 12 Daspro/aritmatika/aritmatika.py
@@ -0,0 +1,9 @@
+def penjumlahan (num1,num2):
+    return num1 + num2
+
+def perpangkatan (num1,num2):
+    return num1 ** num2
+
+def perkalian (num1,num2):
+    return num1 * num2
+
---
+++ Tugas 12 Daspro/ubahBil/ubahBil.py
@@ -0,0 +1,8 @@
+def desimalToBiner(biner):
+    return bin(biner)
+
+def desimalToOktal(oktal):
+    return oct(oktal)
+
+def desimalToHexadesimal(hexa):
+    return hex(hexa)
